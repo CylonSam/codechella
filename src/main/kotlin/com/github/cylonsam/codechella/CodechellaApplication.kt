c4c1ca9a0b29703f46e78c06ee25ddd9685e0824
Samantha L A Silva <samanthaathayde@hotmail.com>
2024-12-12 22:40:36 -0300

    Initial commit

---
+++ src/main/kotlin/com/github/cylonsam/codechella/CodechellaApplication.kt
@@ -0,0 +1,11 @@
+package com.github.cylonsam.codechella
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodechellaApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodechellaApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/cylonsam/codechella/CodechellaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.cylonsam.codechella
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodechellaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
